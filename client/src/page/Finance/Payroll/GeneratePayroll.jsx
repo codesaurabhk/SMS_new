d37b322b4567442530137dfe17df84ee701f9a99
Diwakar Kumar <diwakarkumar9451@gmail.com>
2026-03-03 15:51:25 +0530

    payroll

--- client/src/page/Finance/Payroll/GeneratePayroll.jsx
+++ client/src/page/Finance/Payroll/GeneratePayroll.jsx
@@ -1,16 +1,119 @@
 import React, { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
+import king from "../../../assets/images/king.jpg";
+import lantern from "../../../assets/images/lantern.png";
+
 
 function GeneratePayroll() {
   const [showPayroll, setShowPayroll] = useState(false);
   const [active, setActive] = useState("All");
 
   const baseBtn = "gap-8 ";
-
-  const activeBtn =
-    "bg-[#F5F7F7] text-[#0B3142] text-[16px] font-semibold border border-[#ffffff] rounded-full px-4 py-2 ";
-
+  const activeBtn ="bg-[#F5F7F7] text-[#0B3142] text-[16px] font-semibold border border-[#ffffff] rounded-full px-4 py-2 ";
   const inactiveBtn = "text-[#9EA1A1] text-[16px] font-semibold px-4 py-2";
+
+  const staffData = [
+  {
+    id: 1,
+    name: "Aarav Sharma",
+    image: king,
+    empId: "EMP001",
+    role: "Mathematics Teacher",
+    department: "Academics",
+    paid: "₹45,000",
+    status: "Paid",
+  },
+  {
+    id: 2,
+    name: "Priya Verma",
+    image: lantern,
+    empId: "EMP001",
+    role: "Science Teacher",
+    department: "Academics",
+    paid: "₹42,000",
+    status: "Unpaid",
+  },
+  {
+    id: 3,
+    name: "Rohan Mehta",
+    image: king,
+    empId: "EMP001",
+    role: "Accountant",
+    department: "Finance",
+    paid: "₹38,000",
+    status: "Paid",
+  },
+  {
+    id: 4,
+    name: "Sneha Kapoor",
+    image: lantern,
+    empId: "EMP001",
+    role: "HR Manager",
+    department: "Human Resources",
+    paid: "₹50,000",
+    status: "Paid",
+  },
+  {
+    id: 5,
+    name: "Vikram Singh",
+    image: king,
+    empId: "EMP001",
+    role: "Librarian",
+    department: "Library",
+    paid: "₹28,000",
+    status: "Unpaid",
+  },
+  {
+    id: 6,
+    name: "Ananya Das",
+    image: king,
+    empId: "EMP001",
+    role: "Computer Instructor",
+    department: "IT",
+    paid: "₹47,000",
+    status: "Paid",
+  },
+  {
+    id: 7,
+    name: "Kunal Patel",
+    image: lantern,
+    empId: "EMP001",
+    role: "Sports Coach",
+    department: "Sports",
+    paid: "₹30,000",
+    status: "Pending",
+  },
+  {
+    id: 8,
+    name: "Meera Nair",
+    image: king,
+    empId: "EMP001",
+    role: "Receptionist",
+    department: "Administration",
+    paid: "₹25,000",
+    status: "Paid",
+  },
+  {
+    id: 9,
+    name: "Rahul Yadav",
+    image: king,
+    empId: "EMP001",
+    role: "Transport Manager",
+    department: "Transport",
+    paid: "₹35,000",
+    status: "Pending",
+  },
+  {
+    id: 10,
+    name: "Pooja Iyer",
+    image: lantern,
+    empId: "EMP001",
+    role: "Counselor",
+    department: "Student Welfare",
+    paid: "₹40,000",
+    status: "Unpaid",
+  },
+];
 
   return (
     <div>
@@ -172,6 +275,94 @@ function GeneratePayroll() {
                 <option value="">Active</option>
               </select>
             </div>
+          </div>
+
+          <div className="mt-8 border border-[#e6e6e6] rounded-lg">
+            <table className="w-full">
+              <thead>
+                <tr className="border-b border-[#e6e6e6]">
+                  <th className="px-5 py-4 text-left text-[14px] text-[#1c1c1c] font-semibold flex justify-between items-center">
+                    <span>Staff</span>
+                  </th>
+                  <th className="px-5 py-4 text-left text-[14px] text-[#1c1c1c] font-semibold">
+                    Role
+                  </th>
+                  <th className="px-5 py-4 text-left text-[14px] text-[#1c1c1c] font-semibold">
+                    Department
+                  </th>
+                  <th className="px-5 py-4 text-left text-[14px] text-[#1c1c1c] font-semibold">
+                    Paid
+                  </th>
+                  <th className="px-5 py-4 text-left text-[14px] text-[#1c1c1c] font-semibold">
+                    Status
+                  </th>
+                  <th className="px-5 py-4 text-left text-[14px] text-[#1c1c1c] font-semibold">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {staffData.map((item, index) => ( 
+                  <tr  className="border-b border-[#e6e6e6]">
+                    <td className="px-4 py-3 text-left font-normal text-[#12516E] text-[14px] w-[40%]">
+                      <div className="flex gap-4">
+                        <div className="w-10 h-10 rounded-full overflow-hidden">
+                          <img
+                            src={item.image}
+                            alt=""
+                            className="w-full h-full object-cover"
+                          />
+                        </div>
+
+                        {/* Name + ID */}
+                        <div className="flex flex-col leading-tight">
+                          <span className="text-[#12516E] font-semibold">
+                            {item.name}
+                          </span>
+                          <span className="text-sm text-[#9c9c9c] font-semibold">
+                            {item.empId}
+                          </span>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-4 py-3 text-left font-normal text-[14px]">
+                      {item.role}
+                    </td>
+                    <td className="px-4 py-3 text-left font-normal text-[14px]">
+                      {item.department}
+                    </td>
+                    <td className="px-4 py-3 text-left font-normal text-[14px]">
+                      {item.amountwaived}
+                    </td>
+                    <td className="px-4 py-3 text-left font-normal text-[14px]">
+                      {/* <span
+                        className={`px-2 py-1 rounded ${
+                          item.status === "Active"
+                            ? "bg-[#D4EDDA] text-[#009638]"
+                            : "bg-[#F8D7DA] text-[#C92131]"
+                        }`}
+                      >
+                        {item.status}
+                      </span> */}
+                    </td>
+                    <td className="px-4 py-3 text-left font-normal text-[14px] flex gap-2 items-center text-[#9C9C9C]">
+                      <span className="">
+                        {/* <IoEyeOutline size={18} /> */}
+                      </span>
+                      <span>
+                        {/* <FiRefreshCcw size={18} /> */}
+                      </span>
+                      <span>
+                        {/* <FiEdit size={18} /> */}
+                      </span>
+                      <span>
+                        {/* <RiDeleteBinLine size={18} /> */}
+                      </span>
+                    </td>
+                  </tr>
+                 ))}
+              </tbody>
+            </table>
           </div>
         </div>
       )}
